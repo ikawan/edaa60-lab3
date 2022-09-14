5756bf321dcd3fac079213c0d691ee92937964a7
Ibrahim Aljuboori <alkawwan@gmail.com>
2022-09-14 23:33:16 +0200

    sup

--- hello.scala
+++ hello.scala
@@ -1,9 +1,5 @@
 object HelloWorld {
 def main(args: Array[String]): Unit = {
-<<<<<<< HEAD
-println("Hello, trafalgar law!")
-=======
-println("Hello, luffy!")
->>>>>>> 84c5a81ab57cb08d98b1be7ff562c05a4af66c13
+println("Hello, luffy")
 }
 }
